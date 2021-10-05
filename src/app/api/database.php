5b959aee0d0f25ae20a24e3952ed5910dd068663
= <sebasseb>
2021-10-05 13:40:09 -0300

    conexion php no testeada

--- src/app/api/database.php
+++ src/app/api/database.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
+
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
@@ -10,12 +11,15 @@ define('DB_NAME', 'canchaseb');
 
 function connect()
 {
-    $connect = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-    if (mysqli_connect_errno($connect)) {
-        die("Failed to connect:" . mysqli_connect_error());
-    }
-    mysqli_set_charset($connect, "utf8");
-    return $connect;
+  $connect = mysqli_connect(DB_HOST ,DB_USER ,DB_PASS ,DB_NAME);
+
+  if (mysqli_connect_errno($connect)) {
+    die("Failed to connect:" . mysqli_connect_error());
+  }
+
+  mysqli_set_charset($connect, "utf8");
+
+  return $connect;
 }
 
 $con = connect();
